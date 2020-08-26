fe8899de18f660c6df2ea54941cac3cf5176ca09
kokorinosoba <34607448+kokorinosoba@users.noreply.github.com>
2020-08-27 02:19:46 +0900

    :sparkles: 編集と削除機能もつけた

--- flip/urls.py
+++ flip/urls.py
@@ -6,5 +6,8 @@ app_name = 'flip'
 urlpatterns = [
     path('', views.FlipListView.as_view(), name='index'),
     path('create/', views.FlipCreateView.as_view(), name='create'),
-    path('ajax/', views.ajax, name='ajax')
+    path('update/<int:pk>', views.FlipUpdateView.as_view(), name='update'),
+    path('delete/<int:pk>', views.FlipDeleteView.as_view(), name='delete'),
+    path('delete_all/', views.delete_all, name='delete_all'),
+    path('ajax/', views.ajax, name='ajax'),
 ]
--- flip/views.py
+++ flip/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, UpdateView, DeleteView, ListView
 from django.urls import reverse_lazy
 from .forms import FlipForm
 from .models import Flip
@@ -11,6 +11,11 @@ def ajax(request):
     return JsonResponse({'objects': flips})
 
 
+def delete_all(request):
+    Flip.objects.all().delete()
+    return HttpResponse('削除しました')
+
+
 class FlipCreateView(CreateView):
     model = Flip
     template_name = 'flip/create.html'
@@ -19,6 +24,21 @@ class FlipCreateView(CreateView):
     def get_success_url(self):
         return reverse_lazy('flip:index')
 
+
+class FlipUpdateView(UpdateView):
+    model = Flip
+    template_name = 'flip/update.html'
+    form_class = FlipForm
+
+    def get_success_url(self):
+        return reverse_lazy('flip:index')
+
+
+class FlipDeleteView(DeleteView):
+    model = Flip
+    template_name = 'flip/delete.html'
+    form_class = FlipForm
+    success_url = reverse_lazy('flip:index')
 
 class FlipListView(ListView):
     model = Flip
